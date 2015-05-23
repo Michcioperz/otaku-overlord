7f6c2ac4f220b6d05e05e3240c321108a547e5f0
Michał Sidor (Michcioperz) <michcioperz@gmail.com>
2015-05-24 01:22:02 +0200

    bugfixing, ofc

--- animesrv.py
+++ animesrv.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, redirect, url_for, jsonify, flash
 import anime as animelib
 import subprocess, os, shutil
 app = Flask(__name__)
+app.secret_key = "mou-oshimai-da"
 prefix = '/otaku'
 
 def desc(series):
